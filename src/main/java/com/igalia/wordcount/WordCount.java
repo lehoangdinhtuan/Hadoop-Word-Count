54cd1293c2d4f9c6ec8151602c714c09ba6426e2
tuanle <lehoangdinhtuan@gmail.com>
2019-02-08 13:54:07 +0700

    hihi

--- src/main/java/com/igalia/wordcount/WordCount.java
+++ src/main/java/com/igalia/wordcount/WordCount.java
@@ -22,61 +22,67 @@ import java.util.StringTokenizer;
 public class WordCount {
     private static final Logger logger = LoggerFactory.getLogger(WordCount.class);
 
-	public static class TokenizerMapper
-			extends Mapper<Object, Text, Text, IntWritable>{
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
 
-		private final static IntWritable one = new IntWritable(1);
-		private Text word = new Text();
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
 
-		public void map(Object key, Text value, Context context
-		) throws IOException, InterruptedException {
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
             logger.error("map key {} value {} context {}", key, value, new Gson().toJson(context));
-			StringTokenizer itr = new StringTokenizer(value.toString());
-			while (itr.hasMoreTokens()) {
-				word.set(itr.nextToken());
-				context.write(word, one);
-			}
-		}
-	}
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                context.write(word, one);
+            }
+        }
+    }
 
-	public static class IntSumReducer
-			extends Reducer<Text,IntWritable,Text,IntWritable> {
-		private IntWritable result = new IntWritable();
+    public static class IntSumReducer
+            extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
 
-		public void reduce(Text key, Iterable<IntWritable> values,
-						   Context context
-		) throws IOException, InterruptedException {
-			logger.error("reduce key {} value {} context {}", key, new Gson().toJson(values),  new Gson().toJson(context));
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            logger.error("reduce key {} value {} context {}", key, new Gson().toJson(values), new Gson().toJson(context));
 
-			int sum = 0;
-			for (IntWritable val : values) {
-				sum += val.get();
-			}
-			result.set(sum);
-			context.write(key, result);
-		}
-	}
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
 
-	public static void main(String[] args) throws Exception {
-		Configuration conf = new Configuration();
-		Job job = Job.getInstance(conf, "word count");
-		job.setJarByClass(WordCount.class);
-		job.setMapperClass(TokenizerMapper.class);
-		job.setCombinerClass(IntSumReducer.class);
-		job.setReducerClass(IntSumReducer.class);
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "word count");
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        //additional class handler
+//		job.setMapOutputKeyClass(Key_value.class);
+//		job.setMapOutputValueClass(IntWritable.class);
+//		job.setInputFormatClass(Titanic_input.class);
+//		job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
 //		TEST code check read file
-		FileSystem fs = FileSystem.get(conf);
-		FSDataInputStream inputStream = fs.open(new Path("input/file01"));
-		String out = IOUtils.toString(inputStream,"UTF8");
-		System.out.println(out);
-		inputStream.close();
+        FileSystem fs = FileSystem.get(conf);
+        FSDataInputStream inputStream = fs.open(new Path("input/file01"));
+        String out = IOUtils.toString(inputStream, "UTF8");
+        System.out.println(out);
+        inputStream.close();
 
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
 
-	}
+    }
 }
